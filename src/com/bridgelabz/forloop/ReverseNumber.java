6e75fc58a02c0d36b3c5fbbc23ca7ca5d08b6443
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-19 13:19:15 +0530

    Added :Reverse number

--- src/com/bridgelabz/forloop/ReverseNumber.java
+++ src/com/bridgelabz/forloop/ReverseNumber.java
@@ -1,2 +1,18 @@
-package com.bridgelabz.forloop;public class ReverseNumber {
+package com.bridgelabz.forloop;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter number:");
+        int number=scanner.nextInt();
+        int rem,rev=0;
+
+        for (int i =number; i !=0 ; i=i/10) {
+            rem=i%10;
+            rev=rev*10+rem;
+        }
+        System.out.println("Reverse number is "+rev);
+    }
 }
