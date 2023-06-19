e44eb0e63e50b3c6d7c64159a4c8759bfc6160f2
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-19 13:17:52 +0530

    Added: Check Palindrome number or not

--- src/com/bridgelabz/forloop/PalindromeNumber.java
+++ src/com/bridgelabz/forloop/PalindromeNumber.java
@@ -1,2 +1,23 @@
-package com.bridgelabz.forloop;public class PalindromeNumber {
+package com.bridgelabz.forloop;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter number:");
+        int number=scanner.nextInt();
+        int temp=number;
+        int rem,rev=0;
+
+        for (int i =temp; i !=0 ; i=i/10) {
+            rem=i%10;
+            rev=rev*10+rem;
+        }
+        if (number==rev){
+            System.out.println(number+" is Palindrome number");
+        }else {
+            System.out.println(number+" is not palindrome number");
+        }
+    }
 }
