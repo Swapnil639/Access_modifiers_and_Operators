0778d5a70e04de9a53fc3fb80387d5601285d56b
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-19 13:13:50 +0530

    Added :Natural number

--- src/com/bridgelabz/forloop/NaturalNumber.java
+++ src/com/bridgelabz/forloop/NaturalNumber.java
@@ -1,2 +1,16 @@
-package com.bridgelabz.forloop;public class NaturalNumber {
+package com.bridgelabz.forloop;
+
+import java.util.Scanner;
+
+public class NaturalNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int number = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
 }
