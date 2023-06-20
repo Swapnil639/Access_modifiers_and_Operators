e0c790243de55999f971f5c7ddad2a8765ba05f6
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-20 10:46:30 +0530

    Added :Do the following arithmetic Operations find max and min.

--- src/com/bridgelabz/operator/ArithmeticOperation.java
+++ src/com/bridgelabz/operator/ArithmeticOperation.java
@@ -1,2 +1,23 @@
-package com.bridgelabz.operator;public class ArithmeticOperation {
+package com.bridgelabz.operator;
+
+import java.util.Scanner;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter three number:");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        int op1=a+b*c;
+        int op2=c+a/b;
+        int op3=a%b+c;
+        int op4=a*b+c;
+
+        System.out.println(op1);
+        System.out.println(op2);
+        System.out.println(op3);
+        System.out.println(op4);
+
+    }
 }
