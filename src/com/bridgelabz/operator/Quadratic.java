c4cb4accf962102e91fb576dbcf1a849ec2a4040
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-20 10:49:16 +0530

    Added :find the roots of the equation a*x*x + b*x + c.

--- src/com/bridgelabz/operator/Quadratic.java
+++ src/com/bridgelabz/operator/Quadratic.java
@@ -1,2 +1,19 @@
-package com.bridgelabz.operator;public class Quadratic {
+package com.bridgelabz.operator;
+
+import java.util.Scanner;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+
+        int delta=b*b-4*a*c;
+        double x1=(-b+Math.sqrt(delta))/(2*a);
+        double x2=(-b-Math.sqrt(delta))/(2*a);
+
+        System.out.println(x1);
+        System.out.println(x2);
+    }
 }
