55fb209ea57772c12c0b518f7c851b08d92f5885
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-20 10:43:26 +0530

    Added :Check Vowels or Consonant

--- src/com/bridgelabz/switchstatement/CheckVowelOrConsonant.java
+++ src/com/bridgelabz/switchstatement/CheckVowelOrConsonant.java
@@ -1,2 +1,47 @@
-package com.bridgelabz.switchstatement;public class CheckVowelOrConsonant {
+package com.bridgelabz.switchstatement;
+
+import java.util.Scanner;
+
+public class CheckVowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter character:");
+        char ch = scanner.next().charAt(0);
+
+        switch (ch) {
+            case 'a':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'e':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'i':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'o':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'u':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'A':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'E':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'I':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'O':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'U':
+                System.out.println(ch+" is vowel");
+                break;
+            default:
+                System.out.println(ch+" is Consonant");
+        }
+
+    }
 }
