5d80a93ca789e497935e25ebcb9bf47aab5fdd2a
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-20 10:54:13 +0530

    Added :if day d of month m is between March 20 (m = 3, d=20) and June 20 (m = 6, d = 20), false otherwise

--- src/com/bridgelabz/operator/SpringSeason.java
+++ src/com/bridgelabz/operator/SpringSeason.java
@@ -1,2 +1,20 @@
-package com.bridgelabz.operator;public class SpringSeason {
+package com.bridgelabz.operator;
+
+import java.util.Scanner;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter month number:");
+        int m = scanner.nextInt();
+        System.out.println("Enter date:");
+        int d = scanner.nextInt();
+
+        boolean isSpring = (m == 3 && d == 21 || d == 31 &&
+                m == 4 && d == 1 || d == 30 &&
+                m == 5 && d == 1 || d == 31 &&
+                m == 6 && d == 1 || d == 21);
+
+        System.out.println(isSpring);
+    }
 }
