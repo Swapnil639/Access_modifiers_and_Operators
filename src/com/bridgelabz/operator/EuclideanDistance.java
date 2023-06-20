1d9a0e6325611d01e3d662adf548da048907d1f6
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-20 10:47:47 +0530

    Added :prints the Euclidean distance

--- src/com/bridgelabz/operator/EuclideanDistance.java
+++ src/com/bridgelabz/operator/EuclideanDistance.java
@@ -1,2 +1,14 @@
-package com.bridgelabz.operator;public class EuclideanDistance {
+package com.bridgelabz.operator;
+
+import java.util.Scanner;
+
+public class EuclideanDistance {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int x=scanner.nextInt();
+        int y=scanner.nextInt();
+
+        double distance=Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+        System.out.println(distance);
+    }
 }
