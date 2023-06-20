fb2e6772eb0f00b4799761dd3ce8c6ab814833fe
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-20 10:40:40 +0530

    Added :Sum of natural number

--- src/com/bridgelabz/whileloop/NaturalNumber.java
+++ src/com/bridgelabz/whileloop/NaturalNumber.java
@@ -1,2 +1,18 @@
-package com.bridgelabz.whileloop;public class NaturalNumber {
+package com.bridgelabz.whileloop;
+
+import java.util.Scanner;
+
+public class NaturalNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int number = scanner.nextInt();
+        int sum=0;
+        while (number>0){
+            sum +=number;
+            number--;
+        }
+        System.out.println(sum);
+
+    }
 }
